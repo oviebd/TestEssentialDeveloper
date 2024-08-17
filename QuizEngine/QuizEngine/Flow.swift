1b6d2296f84e0dcccf983904c56242f827a5fa86
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-08-17 07:57:18 +0600

    Quiz Engine With TDD - Add Answer Route

---
+++ QuizEngine/QuizEngine/Flow.swift
@@ -0,0 +1,60 @@
+//
+//  Flow.swift
+//  
+//
+//  Created by Habibur Rahman on 9/8/24.
+//
+
+import Foundation
+
+protocol Router{
+    typealias AnswerCallback = (String) -> Void
+    func routeTo(question: String, answerCallback: @escaping AnswerCallback)
+    func routeTo(result : [String:String])
+    
+}
+
+class Flow {
+    
+    private let router : Router
+    private let questions : [String]
+    private var result : [String:String] = [:]
+    
+    init(questions: [String], router: Router) {
+        self.router = router
+        self.questions = questions
+    }
+    
+    func start(){
+        if let firstQuestion = questions.first {
+            router.routeTo(question: firstQuestion, answerCallback: routeNext(from: firstQuestion))
+        }else{
+            router.routeTo(result: result)
+        }
+          
+    }
+    
+    private func routeNext(from question : String) ->Router.AnswerCallback{
+        return {[weak self] answer in
+            guard let strongSelf = self else {return}
+            
+            if let currentQuestionIndex = strongSelf.questions.firstIndex(of: question){
+                strongSelf.result[question] = answer
+                if currentQuestionIndex+1 < strongSelf.questions.count {
+                    
+                   
+                    
+                    let nextQuestion = strongSelf.questions[currentQuestionIndex + 1]
+                    strongSelf.router.routeTo(question: nextQuestion, answerCallback: strongSelf.routeNext(from: nextQuestion))
+                }else{
+                    strongSelf.router.routeTo(result: strongSelf.result)
+                }
+                
+             
+            }
+            
+        }
+     
+    }
+    
+}
---
+++ QuizEngine/QuizEngineTests/FlowTests.swift
@@ -0,0 +1,110 @@
+//
+//  FlowTests.swift
+//  QuizEngineTests
+//
+//  Created by Habibur Rahman on 9/8/24.
+//
+@testable import QuizEngine
+import Foundation
+import XCTest
+
+
+class FlowTests : XCTestCase {
+   
+    let router = RouterSpy()
+    
+    func test_start_withNoQuestions_doesNotRouteToAnyQuestion(){
+        makeSut(questions: []).start()
+        XCTAssertTrue(router.routedQuestions.isEmpty)
+    }
+
+    
+    func test_start_withOneQuestions_RoutesToCorrectQuestion(){
+        makeSut(questions: ["01"]).start()
+        XCTAssertEqual(router.routedQuestions, ["01"])
+    }
+    
+    func test_start_withOneQuestions_RoutesToCorrectQuestion_2(){
+        makeSut(questions: ["02"]).start()
+        XCTAssertEqual(router.routedQuestions, ["02"])
+    }
+    
+    func test_start_withTwoQuestions_RoutesToFirstQuestion(){
+        makeSut(questions: ["01","02"]).start()
+        XCTAssertEqual(router.routedQuestions, ["01"])
+    }
+    
+    func test_startTwice_withTwoQuestions_RoutesToFirstQuestionTwice(){
+        let sut = makeSut(questions: ["01", "02"])
+        sut.start()
+        sut.start()
+        XCTAssertEqual(router.routedQuestions, ["01", "01"])
+    }
+    
+    
+    func test_startAndAnswerFirstAndSecondQuestion_withThreeQuestions_RoutesToSecondAndThirdQuestion(){
+        
+        let sut = makeSut(questions: ["01", "02", "03"])
+        sut.start()
+        router.answerCallback("A1")
+        router.answerCallback("A2")
+        
+        XCTAssertEqual(router.routedQuestions, ["01", "02", "03"])
+    }
+    
+    
+    func test_startAndAnswerFirstQuestion_withOneQuestions_doesNotRoutesToAnotherQuestion(){
+        
+        let sut = makeSut(questions: ["01"])
+        sut.start()
+        router.answerCallback("AI")
+        
+        XCTAssertEqual(router.routedQuestions, ["01"])
+    }
+    
+    func test_start_withNoQuestions_routesToResult(){
+        makeSut(questions: []).start()
+        XCTAssertEqual(router.routedResults, [:])
+    }
+
+    func test_startAndAnswerFirstQuestion_withOneQuestions_routesToResult(){
+        let sut = makeSut(questions: ["01"])
+        sut.start()
+        router.answerCallback("A1")
+        
+        XCTAssertEqual(router.routedResults, ["01":"A1"])
+    }
+
+    func test_startAndAnswerFirstAndSecondQuestion_withTwoQuestions_routesToResult(){
+        let sut = makeSut(questions: ["01","02"])
+        sut.start()
+        router.answerCallback("A1")
+        router.answerCallback("A2")
+        
+        XCTAssertEqual(router.routedResults, ["01":"A1", "02":"A2"])
+    }
+    
+    // MARK: Helpers
+    func makeSut (questions : [String]) -> Flow {
+        return Flow(questions: questions, router: router)
+    }
+    
+    class RouterSpy : Router {
+      
+        
+
+        var routedQuestions : [String] = []
+        var routedResults : [String:String]? = nil
+        var answerCallback: Router.AnswerCallback = { _ in}
+        
+        func routeTo(question: String, answerCallback: @escaping Router.AnswerCallback) {
+            self.routedQuestions.append(question)
+            self.answerCallback = answerCallback
+        }
+        
+        
+        func routeTo(result: [String : String]) {
+            routedResults = result
+        }
+    }
+}
